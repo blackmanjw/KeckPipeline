59b37fcd4fb8a567312b598d3720f85e636ebbbb
blackmanjw <joshua.blackman@utas.edu.au>
2018-09-18 18:09:55 +1000

    Created file

--- KeckPipeline/calib.py
+++ KeckPipeline/calib.py
@@ -1,175 +1,50 @@
-import os
-from astropy.io import fits
-import pandas as pd
-import numpy as np
-from datetime import datetime, timedelta
+from utilities.utilities import import_user_config
+from tools import movedata
 
-## Rename Files, Move and Create Folders
+if __name__ == "__main__":
+    # Allow user to in CLI optionally overwrite the config values
+    import argparse
+    import textwrap
 
-def movedata(startdate, numdays):  # where startdate is in the form '01-01-2018' and numdays is just an integer.
-    data = []
-    for file in os.listdir("./PLAY"):  # put in your path directory
-        if file.endswith(".fits"):  # what does the file end with?
-            data.append(os.path.join("PLAY", file))
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                     description=textwrap.dedent('''\
+                ***********************************************************************
+                TestyMcTesterson
+                \tfunkytowntesting
+                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                '''))
 
-    # Creating a list of dates starting from the startdate, and lasts a number of days that you input.
-    datelist = pd.date_range(startdate, periods=numdays, freq='D').strftime('%d-%m-%Y').tolist()
+    parser.add_argument('-c', '--config', type=str, nargs=1, help='usage -c /path/to/config.json')
+    parser.add_argument('-l', '--list', type=str, nargs=1, help='usage -l file.list')
+    parser.add_argument('-m', '--movedata', action="store_true")
+    parser.add_argument('-fs', '--flatsubtract', action="store_true")
+    parser.add_argument('-ds', '--darksubtract', action="store_true")
+    parser.add_argument('-dc', '--darkcombine', action="store_true")
+    parser.add_argument('-fc', '--flatcombine', action="store_true")
+    parser.add_argument('-t', '--test', action="store_true")
 
-    # Create folders named after the list of dates.
-    for folder in datelist:
-        if not os.path.exists('datelist'):
-            os.mkdir(os.path.join('PLAY', str(folder)))
+    args = parser.parse_args()
+    print(args)
+    if args.config is None:
+        config = import_user_config('config/my_config.json')
+    else:
+        config = import_user_config(args.config[0])
+    if args.darkcombine:
+        config.update({'dark_combine': True})
+    if args.darksubtract:
+        config.update({'dark_subtract': True})
+    if args.flatcombine:
+        config.update({'flat_combine': True})
+        print('Please combine flats!!!')
+    if args.flatsubtract:
+        config.update({'flat_subtract': True})
+    if args.flatsubtract:
+        config.update({'move_data': True})
+    if args.list:
+        config.update({'list': args.list[0]})
+    if args.test:
+        print('We Win!!!!!')
 
-    n = len(data)
-
-    obj = []
-    itime = []
-    filt = []
-    renamed = []
-    Date_1 = []
-    for i in range(0, n):
-        header = fits.getheader(data[i])
-        obj.append(header['OBJECT'])
-        itime.append(header['ITIME'])
-        filt.append(header['FWINAME'])
-        Name, Date, Number, Ext = data[i].split(".")
-        Date_1.append(datetime.strptime(Date, "%Y%m%d").date())
-
-        renamed.append(('2018/' + 'K' + header['OBJECT'] + header['FWINAME'] + '' + ".fits"))
-
-    # Name,Date,Number,Ext=line.split(".")
-    lists = [data, obj, Date_1, itime, filt, renamed]
-    data_headers = pd.concat([pd.Series(x) for x in lists], axis=1)
-
-    return data_headers
-
-## CREATE MASTER DARK FRAME
-
-def darks():
-    ## Create List of Darks
-
-    darkfiles = []
-    for file in os.listdir("./Darks"):
-        if file.endswith(".fits"):
-            darkfiles.append(os.path.join("Darks", file))
-
-    with open('Darks/dark.list', 'w') as f:
-        for item in darkfiles:
-            f.write("%s\n" % item)
-
-    # Import list of Dark Frames
-
-    darklist = [line.rstrip('\n') for line in open('Darks/dark.list')]
-
-    print("\n" 'DARK Source Files:')
-
-    print("\n".join(darkfiles))
-
-    print("\n" 'DARK Exposure Times:')
-
-    darksexp = []
-    for line in darklist:
-        header = fits.getheader(line)
-        darksexp.append(header['ITIME'])
-        print(header['ITIME'])
-
-    darks = []
-    for file in darklist:
-        darks.append(fits.getdata(file))
-
-    mediandark = np.median(darks, axis=0)
-
-    header = fits.getheader(darklist[0])
-
-    header['HISTORY'] = 'Median combined'
-
-    fits.writeto('dark_300718_30s.fits', mediandark, header, overwrite=True)
-
-    return mediandark
-
-
-## SUBTRACT DARK FRAMES FROM DOME FLATS
-
-## Create List of Dome Flats
-
-def flats():
-    mediandark = darks()
-    domeflatfiles = []
-    for file in os.listdir("./DomeFlats"):
-        if file.endswith(".fits"):
-            domeflatfiles.append(os.path.join("DomeFlats", file))
-
-    print("\n" 'DOME FLAT LampOFF Source Files:')
-    print("\n".join(domeflatfiles))
-
-    with open('DomeFlats/domeflat.list', 'w') as f:
-        for item in domeflatfiles:
-            f.write("%s\n" % item)
-
-    print("\n" 'DOME FLAT Exposure Times:')
-
-    for line in domeflatfiles:
-        header = fits.getheader(line)
-        print(header['ITIME'])
-
-    if not os.path.exists('./Calib'):
-        os.makedirs('./Calib')
-
-    domelist = [line.rstrip('\n') for line in open('DomeFlats/domeflat.list')]
-
-    domeflat_dark = []
-    n = 0
-    for line in domelist:
-        Name, Date, Number, Ext = line.split(".")
-        n = n + 1
-        domeflat_dark.append('Calib/' + Name[10:-3] + '_' + Date + '_' + 'dark' + '_' + str(n) + ".fits")
-
-    print("\n" 'DOME FLATS LampOFF (Bias Subtracted) Calib Output:')
-    print("\n".join(domeflat_dark))
-
-    with open('Calib/domeflat_dark.list', 'w') as f:
-        for item in domeflat_dark:
-            f.write("%s\n" % item)
-
-    n = len(domeflatfiles)
-
-    for i in range(0, n):
-        data, header = fits.getdata(domeflatfiles[i], header=True)
-        domeout = data - mediandark
-        header['HISTORY'] = 'Bias subtracted'
-        fits.writeto(domeflat_dark[i], domeout, header, overwrite=True)
-
-    return domeflat_dark, domeflatfiles
-
-
-## CREATE MASTER DOME FLATS
-
-##Ks BAND
-
-def domeflat():
-    domeflat_dark, domeflatfiles = flats()
-    Date, data_headers = movedata(startdate, numdays)
-    ksflat_onstack = []
-    ksflat_offstack = []
-
-    for file in domeflat_dark:
-        if "LampOn" in files:
-            data, header = fits.getdata(file, header=True)
-            ksflat_onstack.append(data)
-        if "LampOff" in file:
-            data, header = fits.getdata(file, header=True)
-            ksflat_offstack.append(data)
-
-    ksflat_on = np.median(ksflat_onstack, axis=0)
-    ksflat_off = np.median(ksflat_offstack, axis=0)
-
-    ksflat = ksflat_on - ksflat_off
-    m = np.mean(ksflat)
-    ksflat = ksflat / m
-
-    print(ksflat)
-
-    header['HISTORY'] = 'Combined and Normalized Flat Field'
-    fits.writeto('Calib/ksflat' + '_' + Date + ".fits", ksflat, header, overwrite=True)
-
-    return ksflat
+    #movedata('2018-05-05',3)
+    #for key, value in config.items():
+    #	print(f"Key: {key}\t\tValue: {value}")
---
+++ KeckPipeline/tools.py
@@ -0,0 +1,179 @@
+import os
+from astropy.io import fits
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+## Rename Files, Move and Create Folders
+
+def movedata(startdate, numdays):  # where startdate is in the form '01-01-2018' and numdays is just an integer.
+    data = []
+    for file in os.listdir("./Data"):  # put in your path directory
+        if file.endswith(".fits"):  # what does the file end with?
+            data.append(os.path.join("Data", file))
+
+    # Creating a list of dates starting from the startdate, and lasts a number of days that you input.
+    datelist = pd.date_range(startdate, periods=numdays, freq='D').strftime('%d-%m-%Y').tolist()
+
+    print(datelist)
+
+    # Create folders named after the list of dates.
+    for folder in datelist:
+        if os.path.exists('datelist'):
+            break
+        else:
+            os.mkdir(os.path.join('Data', str(folder)))
+
+    n = len(data)
+
+    obj = []
+    itime = []
+    filt = []
+    renamed = []
+    Date_1 = []
+    for i in range(0, n):
+        header = fits.getheader(data[i])
+        obj.append(header['OBJECT'])
+        itime.append(header['ITIME'])
+        filt.append(header['FWINAME'])
+        Name, Date, Number, Ext = data[i].split(".")
+        Date_1.append(datetime.strptime(Date, "%Y%m%d").date())
+
+        renamed.append(('2018/' + 'K' + header['OBJECT'] + header['FWINAME'] + '' + ".fits"))
+
+    # Name,Date,Number,Ext=line.split(".")
+    lists = [data, obj, Date_1, itime, filt, renamed]
+    data_headers = pd.concat([pd.Series(x) for x in lists], axis=1)
+
+    return Date, data_headers
+
+## CREATE MASTER DARK FRAME
+
+def darks():
+    ## Create List of Darks
+
+    darkfiles = []
+    for file in os.listdir("./Darks"):
+        if file.endswith(".fits"):
+            darkfiles.append(os.path.join("Darks", file))
+
+    with open('Darks/dark.list', 'w') as f:
+        for item in darkfiles:
+            f.write("%s\n" % item)
+
+    # Import list of Dark Frames
+
+    darklist = [line.rstrip('\n') for line in open('Darks/dark.list')]
+
+    print("\n" 'DARK Source Files:')
+
+    print("\n".join(darkfiles))
+
+    print("\n" 'DARK Exposure Times:')
+
+    darksexp = []
+    for line in darklist:
+        header = fits.getheader(line)
+        darksexp.append(header['ITIME'])
+        print(header['ITIME'])
+
+    darks = []
+    for file in darklist:
+        darks.append(fits.getdata(file))
+
+    mediandark = np.median(darks, axis=0)
+
+    header = fits.getheader(darklist[0])
+
+    header['HISTORY'] = 'Median combined'
+
+    fits.writeto('dark_300718_30s.fits', mediandark, header, overwrite=True)
+
+    return mediandark
+
+
+## SUBTRACT DARK FRAMES FROM DOME FLATS
+
+## Create List of Dome Flats
+
+def flats():
+    mediandark = darks()
+    domeflatfiles = []
+    for file in os.listdir("./DomeFlats"):
+        if file.endswith(".fits"):
+            domeflatfiles.append(os.path.join("DomeFlats", file))
+
+    print("\n" 'DOME FLAT LampOFF Source Files:')
+    print("\n".join(domeflatfiles))
+
+    with open('DomeFlats/domeflat.list', 'w') as f:
+        for item in domeflatfiles:
+            f.write("%s\n" % item)
+
+    print("\n" 'DOME FLAT Exposure Times:')
+
+    for line in domeflatfiles:
+        header = fits.getheader(line)
+        print(header['ITIME'])
+
+    if not os.path.exists('./Calib'):
+        os.makedirs('./Calib')
+
+    domelist = [line.rstrip('\n') for line in open('DomeFlats/domeflat.list')]
+
+    domeflat_dark = []
+    n = 0
+    for line in domelist:
+        Name, Date, Number, Ext = line.split(".")
+        n = n + 1
+        domeflat_dark.append('Calib/' + Name[10:-3] + '_' + Date + '_' + 'dark' + '_' + str(n) + ".fits")
+
+    print("\n" 'DOME FLATS LampOFF (Bias Subtracted) Calib Output:')
+    print("\n".join(domeflat_dark))
+
+    with open('Calib/domeflat_dark.list', 'w') as f:
+        for item in domeflat_dark:
+            f.write("%s\n" % item)
+
+    n = len(domeflatfiles)
+
+    for i in range(0, n):
+        data, header = fits.getdata(domeflatfiles[i], header=True)
+        domeout = data - mediandark
+        header['HISTORY'] = 'Bias subtracted'
+        fits.writeto(domeflat_dark[i], domeout, header, overwrite=True)
+
+    return domeflat_dark, domeflatfiles
+
+
+## CREATE MASTER DOME FLATS
+
+##Ks BAND
+
+def domeflat():
+    domeflat_dark, domeflatfiles = flats()
+    Date, data_headers = movedata(startdate, numdays)
+    ksflat_onstack = []
+    ksflat_offstack = []
+
+    for file in domeflat_dark:
+        if "LampOn" in files:
+            data, header = fits.getdata(file, header=True)
+            ksflat_onstack.append(data)
+        if "LampOff" in file:
+            data, header = fits.getdata(file, header=True)
+            ksflat_offstack.append(data)
+
+    ksflat_on = np.median(ksflat_onstack, axis=0)
+    ksflat_off = np.median(ksflat_offstack, axis=0)
+
+    ksflat = ksflat_on - ksflat_off
+    m = np.mean(ksflat)
+    ksflat = ksflat / m
+
+    print(ksflat)
+
+    header['HISTORY'] = 'Combined and Normalized Flat Field'
+    fits.writeto('Calib/ksflat' + '_' + Date + ".fits", ksflat, header, overwrite=True)
+
+    return ksflat
