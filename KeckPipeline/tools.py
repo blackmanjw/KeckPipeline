b2f98d2f6f363bdb29ab2c8cf62d34c3dddfb84a
blackmanjw <joshua.blackman@utas.edu.au>
2018-09-30 17:16:00 +1000

    Created

--- KeckPipeline/tools.py
+++ KeckPipeline/tools.py
@@ -1,19 +1,64 @@
+############################################################################
+#                       KECK NIRC2 AO Reduction Tools                      #
+#                                                                          #
+#                             REVISION: 0.0.1                              #
+#                                                                          #
+#                           Joshua Blackman                                #
+#                         Aikaterini Vandorou                              #
+#                        Univeristy of Tasmania                            #
+#                                                                          #
+#                       Last Change: Oct 2018                              #
+############################################################################
+#
+#
+# This is a pipeline to reduce AO images from the NIRC2 imager on KECK II.
+#
+
+
 import os
 from astropy.io import fits
 import astromatic_wrapper as aw
 import glob
 import pandas as pd
 import numpy as np
-from shutil import copyfile
+import shutil
 from datetime import datetime, timedelta
 
-pd.options.display.max_rows = 4000
 
-## Rename Files, Move and Create Folders from the KOA website and sorts the files into Objects, Darks, Flats and Skys.
-## Objects are sorted by object, then date. The calibration files are just sorted by date.
-## Run with "python3 calib.py -r -s <source_folder>" where <soure_folder> is the path to your data you wish to move and rename.
+# ------------------------------ USEFUL FUNCTIONS -----------------------------
+
+def keep_going():
+    """
+        LLALALALALALALALALALALALLALALLALA
+    """
+    answer = input("This script will backup the original folder to dest_dir/Source/** "
+                   "and remove the original folder. It will make copies of the "
+                   "original files and rename them in directories called Darks, Flats, etc. "
+                   "Do you wish to continue? Answer Y or N.")
+
+    if answer == 'Y':
+        print("The script is now running....")
+    else:
+        print("You have chosen to quit this program")
+        raise SystemExit
+
+
+
+# ------------------------------ PIPELINE FUNCTIONS -----------------------------
+
 
 def rename(source_dir,dest_dir):
+    """
+        Rename Files, Move and Create Folders from the KOA website and sorts the files into Objects, Darks, Flats and Skys.
+        Objects are sorted by object, then date. The calibration files are just sorted by date.
+        Run with "python3 calib.py -r -s <source_folder>" where <soure_folder> is the path to your data you wish to move and rename.
+
+    """
+    keep_going()
+
+    ## Backup Original Source Folder
+    shutil.copytree(source_dir, dest_dir + '/Source')
+
     data = []
     for file in os.listdir("./" + source_dir):  # put in your path directory
         if file.endswith(".fits"):  # what does the file end with?
@@ -21,8 +66,7 @@ def rename(source_dir,dest_dir):
 ## This function takes the raw downloads
 
     n = len(data)
-    obj, itime, filt, renamed, datemod, count, flatmod, mod = ([] for i in range(9))
-    os.makedirs(os.path.dirname('Source/', exist_ok=True))
+    obj, itime, filt, renamed, datemod, count, flatmod, mod = ([] for i in range(8))
     for i in range(0, n):
         header = fits.getheader(data[i])
         Name, Date, Number, Ext = data[i].split(".")
@@ -35,22 +79,24 @@ def rename(source_dir,dest_dir):
         if flatmod[i] in flatmod:
             count = flatmod.count(flatmod[i])
         if ('Lamp' in obj[i] or 'Flat' in obj[i]):
-            renamed.append(('Flats/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + str(count) + ".fits"))
-            os.makedirs(os.path.dirname('Flats/' + str(datemod[i]) + '/'), exist_ok=True)
+            renamed.append((dest_dir + '/Flats/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + str(count) + ".fits"))
+            os.makedirs(os.path.dirname(dest_dir + '/Flats/' + str(datemod[i]) + '/'), exist_ok=True)
         elif ('Dark' in obj[i]) or ('dark' in obj[i]):
-            renamed.append(('Darks/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + str(count) + ".fits"))
-            os.makedirs(os.path.dirname('Darks/' + str(datemod[i]) + '/'), exist_ok=True)
+            renamed.append((dest_dir + '/Darks/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + str(count) + ".fits"))
+            os.makedirs(os.path.dirname(dest_dir + '/Darks/' + str(datemod[i]) + '/'), exist_ok=True)
         elif ('Sky' in obj[i]) or ('sky' in obj[i]):
-            renamed.append(('Skys/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + header['FWINAME'] + str(
+            renamed.append((dest_dir + '/Skys/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + header['FWINAME'] + str(
                 count) + ".fits"))
-            os.makedirs(os.path.dirname('Skys/' + str(datemod[i]) + '/'), exist_ok=True)
+            os.makedirs(os.path.dirname(dest_dir + '/Skys/' + str(datemod[i]) + '/'), exist_ok=True)
         else:
-            renamed.append(('Objects/' + header['OBJECT'].upper() + '/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'].upper() +
+            renamed.append((dest_dir + '/Objects/' + header['OBJECT'].upper() + '/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'].upper() +
                             header['FWINAME'] + str(
                         count) + ".fits"))
-            os.makedirs(os.path.dirname('Objects/' + header['OBJECT'] + '/' + str(datemod[i]) + '/'), exist_ok=True)
-        copyfile(data[i],)
+            os.makedirs(os.path.dirname(dest_dir + '/Objects/' + header['OBJECT'].upper() + '/' + str(datemod[i]) + '/'), exist_ok=True)
         os.rename(data[i], renamed[i])
+
+    ## REMOVE LEFT OVER original Folders
+    shutil.rmtree(source_dir)
 
     # Name,Date,Number,Ext=line.split(".")
     lists = [data, mod, datemod, itime, flatmod, renamed]
@@ -58,6 +104,45 @@ def rename(source_dir,dest_dir):
     print(data_headers)
 
     return data_headers
+
+### Combine Dark Frames
+
+def darkcombine():
+    """
+        LLALALALALALALALALALALALLALALLALA
+    """
+    darkfiles=darklist(dir)
+
+    dark_list = [line.rstrip('\n') for line in open('Darks/dark.list')]
+
+    print("\n" 'DARK Source Files:')
+
+    print("\n".join(darkfiles))
+
+    print("\n" 'DARK Exposure Times:')
+
+    darksexp = []
+    for line in dark_list:
+        header = fits.getheader(line)
+        darksexp.append(header['ITIME'])
+        print(header['ITIME'])
+
+    darks = []
+    for file in dark_list:
+        darks.append(fits.getdata(file))
+
+    mediandark = np.median(darks, axis=0)
+
+    header = fits.getheader(dark_list[0])
+
+    header['HISTORY'] = 'Median combined'
+
+    fits.writeto('dark_300718_30s.fits', mediandark, header, overwrite=True)
+
+    return mediandark
+
+
+
 
 ## Create *.list file of darkframes
 
@@ -103,40 +188,6 @@ def darklist(dir):
                 f.write("%s\n" % item)
 
     return darkfiles
-
-### Combine Dark Frames
-
-def darkcombine():
-
-    darkfiles=darklist(dir)
-
-    dark_list = [line.rstrip('\n') for line in open('Darks/dark.list')]
-
-    print("\n" 'DARK Source Files:')
-
-    print("\n".join(darkfiles))
-
-    print("\n" 'DARK Exposure Times:')
-
-    darksexp = []
-    for line in dark_list:
-        header = fits.getheader(line)
-        darksexp.append(header['ITIME'])
-        print(header['ITIME'])
-
-    darks = []
-    for file in dark_list:
-        darks.append(fits.getdata(file))
-
-    mediandark = np.median(darks, axis=0)
-
-    header = fits.getheader(dark_list[0])
-
-    header['HISTORY'] = 'Median combined'
-
-    fits.writeto('dark_300718_30s.fits', mediandark, header, overwrite=True)
-
-    return mediandark
 
 
 ## SUBTRACT DARK FRAMES FROM DOME FLATS
@@ -244,9 +295,3 @@ def domeflat():
 
 ## COMBINE SKY FLATS
 
-def skyflat()
-
-
-
-
-    return
