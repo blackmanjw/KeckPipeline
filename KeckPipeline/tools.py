cb15fde62101019d8bf34ced03f32411a8d6235a
blackmanjw <joshua.blackman@utas.edu.au>
2018-10-22 16:11:53 +1100

    Created

--- KeckPipeline/tools.py
+++ KeckPipeline/tools.py
@@ -32,6 +32,8 @@ from astropy.io.fits import getdata
 import astromatic_wrapper as aw
 import numpy as np
 from distutils import dir_util
+from photutils import make_source_mask
+from astropy.stats import sigma_clipped_stats
 
 warnings.filterwarnings("ignore")
 
@@ -58,7 +60,7 @@ def keep_going(text="Do you wish to continue? Answer Y or N."):
         raise SystemExit
 
 
-def swarp(files, output='output.fits', celestial_type='PIXEL'):
+def swarp(files, output='output.fits', celestial_type='EQUATORIAL'):
     """
         Run SWARP!
 
@@ -79,6 +81,8 @@ def swarp(files, output='output.fits', celestial_type='PIXEL'):
             'IMAGEOUT_NAME': output,
             'RESCALE_WEIGHTS': 'N',
             'RESAMPLE': 'N',
+            'COMBINE_TYPE': 'MEDIAN',
+            'PIXEL_SCALE': '0',
             'CELESTIAL_TYPE': celestial_type,
             'INTERPOLATE': 'N',
             'BLANK_BADPIXELS': 'N',
@@ -121,7 +125,7 @@ def sex(files, output='output.cat', phot_aperture=10, back_size=256):
             'BACK_SIZE': str(back_size),
         },
         # Output parameters
-        'params': ['NUMBER', 'XWIN_IMAGE', 'YWIN_IMAGE', 'MAG_AUTO', 'FLUX_APER', 'FLUXERR_APER'],
+        'params': ['NUMBER', 'BACKGROUND', 'XWIN_IMAGE', 'YWIN_IMAGE', 'MAG_AUTO', 'FLUX_APER', 'FLUXERR_APER'],
         'temp_path': '.',
         'config_file': 'config/config.sex'
     }
@@ -130,9 +134,34 @@ def sex(files, output='output.cat', phot_aperture=10, back_size=256):
     sex.run(files)
 
 
+from photutils import make_source_mask
+from astropy.stats import sigma_clipped_stats
+
+
+def backgroundestimate(file, size=10):
+    """
+        This function estimates the background of a fits images via iteration and masking of any sources.
+
+        Required imports: from photutils import make_source_mask
+                          from astropy.stats import sigma_clipped_stats
+
+        Parameters
+        ----------
+        file:
+            Path to fits file.
+        output:
+            The mean background count.
+    """
+    data = CCDData.read(file, unit="adu")
+    mask = make_source_mask(data, snr=2, npixels=5, dilate_size=size)
+    mean, median, std = sigma_clipped_stats(data, sigma=3.0, mask=mask)
+    print(mean)
+    return mean
+
+
 # ------------------------------ PIPELINE FUNCTIONS -----------------------------
 
-def rename(source_dir,dest_dir):
+def rename(source_dir, dest_dir):
     """
         This function takes the downloaded files from the KOA website, renames them and sorts into folders named Objects, Darks, Flats and Skys.
         Objects are sorted by object name (read from the fits header), then date. Calibration files are sorted by date.
@@ -155,7 +184,8 @@ def rename(source_dir,dest_dir):
         data_headers
             Table with columns showing input files, output files, object name and exposure time.
     """
-    keep_going(text="This script will backup the original folder to dest_dir/Source/** and remove the original folder. It will make copies of the  original files and rename them in directories called Darks, Flats, etc. Do you wish to continue? Answer Y or N.")
+    keep_going(
+        text="This script will backup the original folder to dest_dir/Source/** and remove the original folder. It will make copies of the  original files and rename them in directories called Darks, Flats, etc. Do you wish to continue? Answer Y or N.")
 
     ## Backup Original Source Folder
     dir_util.copy_tree(source_dir, dest_dir + '/Source')
@@ -179,20 +209,26 @@ def rename(source_dir,dest_dir):
         if flatmod[i] in flatmod:
             count = flatmod.count(flatmod[i])
         if ('Lamp' in obj[i] or 'Flat' in obj[i]):
-            renamed.append((dest_dir + '/Flats/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + str(count) + ".fits"))
+            renamed.append(
+                (dest_dir + '/Flats/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + str(count) + ".fits"))
             os.makedirs(os.path.dirname(dest_dir + '/Flats/' + str(datemod[i]) + '/'), exist_ok=True)
         elif ('Dark' in obj[i]) or ('dark' in obj[i]):
-            renamed.append((dest_dir + '/Darks/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + str(count) + ".fits"))
+            renamed.append(
+                (dest_dir + '/Darks/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + str(count) + ".fits"))
             os.makedirs(os.path.dirname(dest_dir + '/Darks/' + str(datemod[i]) + '/'), exist_ok=True)
         elif ('Sky' in obj[i]) or ('sky' in obj[i]):
-            renamed.append((dest_dir + '/Skys/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + header['FWINAME'] + str(
-                count) + ".fits"))
+            renamed.append(
+                (dest_dir + '/Skys/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'] + header['FWINAME'] + str(
+                    count) + ".fits"))
             os.makedirs(os.path.dirname(dest_dir + '/Skys/' + str(datemod[i]) + '/'), exist_ok=True)
         else:
-            renamed.append((dest_dir + '/Objects/' + header['OBJECT'].upper() + '/' + str(datemod[i]) + '/' + 'K' + header['OBJECT'].upper() +
+            renamed.append((dest_dir + '/Objects/' + header['OBJECT'].upper() + '/' + str(datemod[i]) + '/' + 'K' +
+                            header['OBJECT'].upper() +
                             header['FWINAME'] + str(
                         count) + ".fits"))
-            os.makedirs(os.path.dirname(dest_dir + '/Objects/' + header['OBJECT'].upper() + '/' + str(datemod[i]) + '/'), exist_ok=True)
+            os.makedirs(
+                os.path.dirname(dest_dir + '/Objects/' + header['OBJECT'].upper() + '/' + str(datemod[i]) + '/'),
+                exist_ok=True)
         os.rename(data[i], renamed[i])
 
     ## REMOVE LEFT OVER original Folders
@@ -201,11 +237,12 @@ def rename(source_dir,dest_dir):
     # Name,Date,Number,Ext=line.split(".")
     lists = [data, mod, datemod, itime, flatmod, renamed]
     data_headers = pd.concat([pd.Series(x) for x in lists], axis=1)
-    print(data_headers)
+    # print(data_headers)
 
     return data_headers
 
-### Combine Dark Frames
+
+# -----------------------------
 
 def darkcombine(darks_dir='Darks/'):
     """
@@ -255,8 +292,8 @@ def darkcombine(darks_dir='Darks/'):
         if dark60:
             swarp(dark60, output=darks_dir + 'Dark60sec' + d.split("-")[1] + d.split("-")[2][:-1] + '.fits')
 
-    print('DONE')
 
+# -----------------------------
 
 def darksubtract(dir='Flats', master_dark='Darks/Dark60sec0807.fits'):
     """
@@ -279,11 +316,11 @@ def darksubtract(dir='Flats', master_dark='Darks/Dark60sec0807.fits'):
     #    raise SystemExit
 
     if dir == "Flats":
-        dir = 'Flats/*'
+        dir = 'Flats/*/'
     elif dir == "Skys":
-        dir = 'Skys/*'
-    elif dir =="Objects":
-        dir = 'Objects/*/*'
+        dir = 'Skys/*/'
+    elif dir == "Objects":
+        dir = 'Objects/*/*/'
 
     mdark = CCDData.read(master_dark, unit="adu")
 
@@ -303,10 +340,13 @@ def darksubtract(dir='Flats', master_dark='Darks/Dark60sec0807.fits'):
             flats = CCDData(data=flat.data.astype('float32'), meta=meta, unit="adu")
             dflat = (ccdproc.subtract_dark(flats, mdark, exposure_time='ITIME',
                                            exposure_unit=u.second,
-                                           add_keyword={'HISTORY': 'Dark Subtracted' 'OBSDATE': flat.header['DATE-OBS']},
+                                           add_keyword={'HISTORY': 'Dark Subtracted',
+                                                        'OBSDATE': flat.header['DATE-OBS']},
                                            scale=True))
             dflat.write(directory + '/d' + fname, overwrite=True)
 
+
+# -----------------------------
 
 def flatcombine(dir='Flats/*/dark_subtracted/'):
     """
@@ -355,6 +395,9 @@ def flatcombine(dir='Flats/*/dark_subtracted/'):
         swarpfilter(d, dir, directory, images, keys, filter='J', lamp='off', camera='wide', done='Dark Subtracted',
                     output='cKWideLampOffJ')
 
+
+# -----------------------------
+
 def flatprocess(dir='Flats/*/swarped/'):
     """
 
@@ -366,7 +409,7 @@ def flatprocess(dir='Flats/*/swarped/'):
         -------
         ????
     """
-
+    print(glob(dir))
     for d in glob(dir):
 
         keys = ['OBJECT', 'CAMNAME', 'FWINAME', 'ITIME', 'OBSDATE', 'FLSPECTR', 'HISTORY']
@@ -381,7 +424,6 @@ def flatprocess(dir='Flats/*/swarped/'):
                 meta = hdu.header
                 meta['filename'] = fname
                 flats.append(ccdproc.CCDData(data=hdu.data.astype('float32'), meta=meta, unit="adu"))
-            print(len(flats))
             if flats:
                 MasterFlat = (ccdproc.subtract_dark(flats[1], flats[0], exposure_time='ITIME',
                                                     exposure_unit=u.second,
@@ -397,7 +439,9 @@ def flatprocess(dir='Flats/*/swarped/'):
                 flats[:] = []
 
 
-def flatcorrect(dir='Skys/*/dark_subtracted/', flatdate='2018-08-07'):
+# -----------------------------
+
+def flatcorrect(dir='Skys', flatdate='2018-08-07'):
     """
 
         Parameters
@@ -408,6 +452,11 @@ def flatcorrect(dir='Skys/*/dark_subtracted/', flatdate='2018-08-07'):
         -------
         ????
     """
+
+    if dir == "Skys":
+        dir = 'Skys/*/dark_subtracted/'
+    elif dir == "Objects":
+        dir = 'Objects/*/*/dark_subtracted/'
 
     keys = ['OBJECT', 'CAMNAME', 'FWINAME', 'ITIME', 'OBSDATE', 'FLSPECTR', 'HISTORY']
     flatfiles = ImageFileCollection('Flats', keywords=keys)
@@ -429,7 +478,11 @@ def flatcorrect(dir='Skys/*/dark_subtracted/', flatdate='2018-08-07'):
 
     for d in glob(dir):
 
-        directory = "/".join(d.split('/')[0:2]) + '/flat_corrected'
+        if dir == "Skys/*/dark_subtracted/":
+            directory = "/".join(d.split('/')[0:2]) + '/flat_corrected'
+        elif dir == "Objects/*/*/dark_subtracted/":
+            directory = "/".join(d.split('/')[0:3]) + '/flat_corrected'
+
         if not os.path.exists(directory):
             os.makedirs(directory)
 
@@ -469,7 +522,9 @@ def flatcorrect(dir='Skys/*/dark_subtracted/', flatdate='2018-08-07'):
                     cflat.write(directory + '/f' + fname, overwrite=True)
 
 
-def skycombine(dir='Skys/*/flat_corrected/'):
+# -----------------------------
+
+def skycombine(dir='Objects'):
     """
         This function combines the skys from files in a give directory, after they have been dark subtracted and flat
         corrected.
@@ -482,6 +537,10 @@ def skycombine(dir='Skys/*/flat_corrected/'):
         -------
         ????
     """
+
+    if dir == "Objects":
+        dir = 'Objects/*/*/flat_corrected/'
+
     for d in glob(dir):
 
         directory = "/".join(d.split('/')[0:2]) + '/swarped'
@@ -508,6 +567,82 @@ def skycombine(dir='Skys/*/flat_corrected/'):
         swarpfilter(d, dir, directory, images, keys, filter='Lp', lamp='*', camera='wide', done='Dark Subtracted',
                     output='cKSkyNarrowLp')
 
+
+# ---------------------------
+
+def skycorrect(dir='Objects'):
+    """
+        This function applys the sky correction to the object files. This takes the form:
+
+        OBJECT - SKY (mean object background / mean sky background)
+
+        Parameters
+        ----------
+        dir: str
+            Target directory containing subdirectories for each object.
+
+        Returns
+        -------
+        ????
+    """
+
+    if dir == "Objects":
+        dir = 'Objects/*/*/flat_corrected/'
+
+    keys = ['OBJECT', 'CAMNAME', 'FWINAME', 'ITIME', 'OBSDATE', 'FLSPECTR', 'HISTORY']
+    cameras = ['wide', 'narrow']
+    filters = ['Ks', 'H', 'J']
+    skys = {}
+    objects = {}
+
+    ## Import MASTER SKYS from swarped directory for all date folders under Skys/*
+
+    for d in glob('Skys/*/swarped'):
+
+        files = ImageFileCollection(d, keywords=keys, glob_include='*.fits')
+
+        for camera, filter in product(cameras, filters):
+
+            for hdu, fname in files.hdus(CAMNAME=camera, FWINAME=filter, return_fname=True):
+                meta = hdu.header
+                meta['filename'] = fname
+                skys[fname[:-5] + meta['OBSDATE'].split("-")[1] + meta['OBSDATE'].split("-")[2]] = [
+                    ccdproc.CCDData(data=hdu.data.astype('float32'), meta=meta, unit="adu"),
+                    backgroundestimate(d + '/' + fname)]
+                print(skys)
+    ## APPLY SKY CORRECTION to Images
+
+
+''' 
+    for d in glob(dir):
+
+        directory = "/".join(d.split('/')[0:3]) + '/sky_corrected'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        images = ImageFileCollection(d, keywords=keys, glob_include='*.fits')
+
+        for camera, filter in product(cameras, filters):
+
+            for hdu, fname in images.hdus(CAMNAME=camera, FWINAME=filter, return_fname=True):
+                meta = hdu.header
+                meta['filename'] = fname
+                objects[fname[:-5]+ meta['OBSDATE'].split("-")[1] + meta['OBSDATE'].split("-")[2]] = [ccdproc.CCDData(data=hdu.data.astype('float32'), meta=meta, unit="adu"),backgroundestimate(d + '/' + fname)]
+                print(objects)
+                #MasterFlat.data = MasterFlat / np.ma.average(MasterFlat)
+                #MasterFlat.write(
+                #    'Flats/KFlat' + MasterFlat.meta['CAMNAME'].title() + MasterFlat.meta['FWINAME'].title() +
+                 #   MasterFlat.meta['OBSDATE'].split("-")[1] + MasterFlat.meta['OBSDATE'].split("-")[2] + '.fits',
+                 #   overwrite=True)
+               # flats[:] = []
+
+'''
+
+
+# Normalize Flat
+# MasterFlat.data = MasterFlat / np.ma.average(MasterFlat)
+
+# backgroundestimate(file='Objects/OB171434/2018-08-07/KOB171434Ks1.fits')
 
 # ------------------------------ REQUIRED FUNCTIONS -----------------------------
 
